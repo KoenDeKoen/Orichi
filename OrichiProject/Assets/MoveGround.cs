95806fb06ae8cca6785f8186271c851637c94a7f
KoenDeKoen <koen.brouwers@live.nl>
2015-09-17 14:43:02 +0200

    StartAtMovingAndSpawningWorking

---
+++ OrichiProject/Assets/MoveGround.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGround : MonoBehaviour {
+
+	// Use this for initialization
+	//public GameObject floorandshizzle;
+	public SpawnGroundAndProps spawngroundandprops;
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		moveGround ();
+		//Debug.Log (floorandshizzle.transform.position.x + floorandshizzle.transform.localScale.x / 2);
+	}
+
+	public void moveGround()
+	{
+		for(int i = 0; i < spawngroundandprops.returnFloors().Count; i++)
+		{
+			if(Input.GetKeyDown("a"))
+			{
+				Vector3 tempvector;
+				tempvector.y = spawngroundandprops.returnFloors()[i].transform.position.y;
+				tempvector.z = spawngroundandprops.returnFloors()[i].transform.position.z;
+				tempvector.x = spawngroundandprops.returnFloors()[i].transform.position.x - 5;
+				
+				spawngroundandprops.returnFloors()[i].transform.position = Vector3.MoveTowards(spawngroundandprops.returnFloors()[i].transform.position, tempvector, Time.deltaTime * 5);
+			}
+		}
+
+	}
+}
--- OrichiProject/Assets/SpawnGroundAndProps.cs
+++ OrichiProject/Assets/SpawnGroundAndProps.cs
@@ -7,32 +7,60 @@ public class SpawnGroundAndProps : MonoBehaviour
 
 	private List<GameObject> floors;
 	public Sprite floorsprite;
+	Vector3 position;
 	// Use this for initialization
 	void Start () 
 	{
-		spawnFloor ();
+		floors = new List<GameObject> ();
+		position = new Vector3 (0, (float)-2.23, 0);
+		spawnFloor (position);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		checkForNextSpawn ();
 	}
 
-	public void  spawnFloor()
+	public void  spawnFloor(Vector3 spawnpos)
 	{
 		GameObject floor = new GameObject();
 		floor.AddComponent<SpriteRenderer> ();
 		floor.GetComponent<SpriteRenderer> ().sprite = floorsprite;
-		Vector3 spawnposition = new Vector3 (0, (float)-2.23, 0);
+		Vector3 spawnposition = spawnpos;
 		floor.transform.position = spawnposition;
+		floors.Add (floor);
+	}
+
+	public void despawnFloor(GameObject floortodespawn)
+	{
+		floors.Remove (floortodespawn);
+		Destroy (floortodespawn);
 	}
 
 	public void checkForNextSpawn()
 	{
-		foreach(GameObject g in floors)
+		Vector3 endofspriteposleft = new Vector3 (0, 0, 0);
+		Vector3 endofspriteposright = new Vector3 (0, 0, 0);
+		for(int i = 0; i < floors.Count; i++)
 		{
+			endofspriteposleft.x = floors[i].GetComponent<SpriteRenderer>().bounds.min.x;
+			endofspriteposright.x = floors[i].GetComponent<SpriteRenderer>().bounds.max.x;
+			if(endofspriteposright.x <= (float)9 && floors.Count < 2)
+			{
+				position.x = endofspriteposright.x + (floors[i].GetComponent<SpriteRenderer>().bounds.extents.x)-(float)+0.1;//+0.1 omdat LOGICS
+				spawnFloor(position);
 
+			}
+			if(endofspriteposright.x <= (float)-9)
+			{
+				despawnFloor(floors[i]);
+			}
 		}
+	}
+
+	public List<GameObject> returnFloors()
+	{
+		return floors;
 	}
 }
