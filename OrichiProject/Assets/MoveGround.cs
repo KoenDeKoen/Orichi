437843244f35a61b48b1c1e481ee99a90765be49
KoenDeKoen <koen.brouwers@live.nl>
2015-09-17 16:38:00 +0200

    AlphaGamePlayFinished

--- OrichiProject/Assets/MoveGround.cs
+++ OrichiProject/Assets/MoveGround.cs
@@ -14,7 +14,7 @@ public class MoveGround : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		moveGround ();
+		//moveGround ();
 		//Debug.Log (floorandshizzle.transform.position.x + floorandshizzle.transform.localScale.x / 2);
 	}
 
@@ -22,15 +22,11 @@ public class MoveGround : MonoBehaviour {
 	{
 		for(int i = 0; i < spawngroundandprops.returnFloors().Count; i++)
 		{
-			if(Input.GetKeyDown("a"))
-			{
-				Vector3 tempvector;
-				tempvector.y = spawngroundandprops.returnFloors()[i].transform.position.y;
-				tempvector.z = spawngroundandprops.returnFloors()[i].transform.position.z;
-				tempvector.x = spawngroundandprops.returnFloors()[i].transform.position.x - 5;
-				
-				spawngroundandprops.returnFloors()[i].transform.position = Vector3.MoveTowards(spawngroundandprops.returnFloors()[i].transform.position, tempvector, Time.deltaTime * 5);
-			}
+			Vector3 tempvector;
+			tempvector.y = spawngroundandprops.returnFloors()[i].transform.position.y;
+			tempvector.z = spawngroundandprops.returnFloors()[i].transform.position.z;
+			tempvector.x = spawngroundandprops.returnFloors()[i].transform.position.x - 5;
+			spawngroundandprops.returnFloors()[i].transform.position = Vector3.MoveTowards(spawngroundandprops.returnFloors()[i].transform.position, tempvector, Time.deltaTime * 5);
 		}
 
 	}
--- OrichiProject/Assets/SpawnGroundAndProps.cs
+++ OrichiProject/Assets/SpawnGroundAndProps.cs
@@ -6,11 +6,15 @@ public class SpawnGroundAndProps : MonoBehaviour
 {
 
 	private List<GameObject> floors;
+	private List<Vector3> decorationpawnpositions;
+	public Sprite bushsprite;
 	public Sprite floorsprite;
 	Vector3 position;
 	// Use this for initialization
 	void Start () 
 	{
+		decorationpawnpositions = new List<Vector3> ();
+		addSpawnableDecorationSpots ();
 		floors = new List<GameObject> ();
 		position = new Vector3 (0, (float)-2.23, 0);
 		spawnFloor (position);
@@ -30,6 +34,8 @@ public class SpawnGroundAndProps : MonoBehaviour
 		Vector3 spawnposition = spawnpos;
 		floor.transform.position = spawnposition;
 		floors.Add (floor);
+		placeGrass (floor);
+		addSpawnableDecorationSpots ();
 	}
 
 	public void despawnFloor(GameObject floortodespawn)
@@ -50,7 +56,6 @@ public class SpawnGroundAndProps : MonoBehaviour
 			{
 				position.x = endofspriteposright.x + (floors[i].GetComponent<SpriteRenderer>().bounds.extents.x)-(float)+0.1;//+0.1 omdat LOGICS
 				spawnFloor(position);
-
 			}
 			if(endofspriteposright.x <= (float)-9)
 			{
@@ -62,5 +67,34 @@ public class SpawnGroundAndProps : MonoBehaviour
 	public List<GameObject> returnFloors()
 	{
 		return floors;
+	}
+
+	public void placeGrass(GameObject parentground)
+	{
+		Vector3 position = new Vector3(parentground.transform.position.x,(float)-1.08,0);
+		Vector3 positiontoremove = new Vector3 (0, 0, 0);
+
+		for(int i = 0; i < Random.Range(3,6); i++)
+		{
+			GameObject grass = new GameObject();
+			grass.AddComponent<SpriteRenderer>();
+			grass.GetComponent<SpriteRenderer>().sprite = bushsprite;
+			positiontoremove = decorationpawnpositions[Random.Range(0,decorationpawnpositions.Count)];
+			position.x += positiontoremove.x;
+			decorationpawnpositions.Remove(positiontoremove);
+			grass.transform.parent = parentground.transform;
+			grass.transform.position = position;
+			grass.GetComponent<SpriteRenderer>().sortingOrder = 2;
+		}
+	}
+
+	public void addSpawnableDecorationSpots()
+	{
+		Vector3 spot = new Vector3 (0, (float)-2.23, 0);
+		for(int i = -5; i < 5; i += 2)
+		{
+			spot.x = i;
+			decorationpawnpositions.Add (spot);
+		}	
 	}
 }
