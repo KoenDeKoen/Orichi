8e9b803e468c4ed334b2edafbddbbbd631f4775f
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-09-24 11:27:15 +0200

    Animation Works

---
+++ OrichiProject/Assets/AnimationScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+
+	public Animator m_ani;
+
+	public void Switch() {
+		m_ani.SetBool("Switch", false);
+	}
+}
