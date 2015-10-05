c5703f3ebf2e16490fdd390490daa72868b05415
KoenDeKoen <koen.brouwers@live.nl>
2015-10-05 11:17:42 +0200

    BasicButtonSupport

---
+++ OrichiProject/Assets/BasicButtons.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicButtons : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public SceneLoader sceneloader;
+	public bool ismenu;
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		checkForButtonPresses ();
+	}
+
+	private void checkForButtonPresses()
+	{
+		if(Input.GetKeyDown("q"))
+		{
+			Application.Quit();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape) && !ismenu)
+		{
+			sceneloader.loadScene("MainMenu");
+		}
+	}
+}
