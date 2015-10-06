d26ffe88a0a729eb3c73cf7f82540efb45da026f
universo5 <raphaelvakkerveken@hotmail.com>
2015-10-06 14:12:00 +0200

    progressbar finished

--- OrichiProject/Assets/ProgressBar.cs
+++ OrichiProject/Assets/ProgressBar.cs
@@ -2,43 +2,36 @@
 using System.Collections;
 
 public class ProgressBar : MonoBehaviour {
-	
-	public Transform startPoint;
-	public Transform endPoint;
-	
-	public GameObject indicator;
-	float totalexercises = 50;
-	float currentexercises = 25;
-	// Update is called once per frame
+	public float barDisplay; //current progress
+	public Vector2 pos = new Vector2(20,40);
+	public Vector2 size = new Vector2(60,20);
+	public Texture2D emptyTex;
+	public Texture2D fullTex;
+	public float progresspercentage;
 
-	void Start() {
-
+	void OnGUI() {
+		//draw the background:
+		GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+		GUI.Box(new Rect(0,0, size.x, size.y), emptyTex);
+		
+		//draw the filled-in part:
+		GUI.BeginGroup(new Rect(0,0, size.x * barDisplay, size.y));
+		GUI.Box(new Rect(0,0, size.x, size.y), fullTex);
+		GUI.EndGroup();
+		GUI.EndGroup();
 	}
-	void Update () {
-//		float currentexercises = MechanicController.stepstaken;
-	//	float totalexercises = Custominput.exercises;
 
-		//lengte tussen begin en eind
-		float distanceLevel = (endPoint.position.x ) - startPoint.position.x;
-		
-		//percentage completion tussen totale exercises
-		float curIndicator = currentexercises / totalexercises * 100;
+
+	void Update() {
 	
-		
-		//give indicator their place
-		indicator.transform.localPosition = new Vector3 (distanceLevel * curIndicator / 100, 0, 0);
+		progresspercentage = MechanicController.stepstaken / Custominput.exercises;
 
+
+		//for this example, the bar display is linked to the current time,
+		//however you would set this value based on your desired display
+		//eg, the loading progress, the player's health, or whatever.
+		barDisplay = progresspercentage;
+		//        barDisplay = MyControlScript.staticHealth;
+		Debug.Log (barDisplay);
 	}
 }
-
-
-// de totale exercises 
-// je current exercises
-// afstand van begin tot eind
-// als current exercises 0 is is afstand 0
-// als current exercises totale exercises is is afstand 100
-//waiting for internet...
-//...
-//...
-//...
-//error 404 internet not found
