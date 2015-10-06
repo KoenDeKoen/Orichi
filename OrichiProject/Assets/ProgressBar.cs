ad0b9291ccf9580a22941f0a880677ac310e2e27
universo5 <raphaelvakkerveken@hotmail.com>
2015-10-06 12:47:39 +0200

    progressbarstart

---
+++ OrichiProject/Assets/ProgressBar.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+	
+	public Transform startPoint;
+	public Transform endPoint;
+	
+	public GameObject indicator;
+	float totalexercises = 50;
+	float currentexercises = 25;
+	// Update is called once per frame
+
+	void Start() {
+
+	}
+	void Update () {
+//		float currentexercises = MechanicController.stepstaken;
+	//	float totalexercises = Custominput.exercises;
+
+		//lengte tussen begin en eind
+		float distanceLevel = (endPoint.position.x ) - startPoint.position.x;
+		
+		//percentage completion tussen totale exercises
+		float curIndicator = currentexercises / totalexercises * 100;
+	
+		
+		//give indicator their place
+		indicator.transform.localPosition = new Vector3 (distanceLevel * curIndicator / 100, 0, 0);
+
+	}
+}
+
+
+// de totale exercises 
+// je current exercises
+// afstand van begin tot eind
+// als current exercises 0 is is afstand 0
+// als current exercises totale exercises is is afstand 100
+//waiting for internet...
+//...
+//...
+//...
+//error 404 internet not found
