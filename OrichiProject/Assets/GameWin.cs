3dc4f119e4f3b25a7ca05bef54ff20c3d3c054af
universo5 <raphaelvakkerveken@hotmail.com>
2015-10-09 14:46:02 +0200

    uhoh

---
+++ OrichiProject/Assets/GameWin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (MechanicController.stepstaken == Custominput.exercises + 1) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
